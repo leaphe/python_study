1909617634596341bb18fa54644c1ea6298d5405
leaphe <13918825181@qq.com>
2019-06-13 18:46:09 +0800

    Davis

---
+++ python-lists.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+list1 = ['physics', 'chemistry', 1997, 2000]
+list2 = [1, 2, 3, 4, 5, 6, 7 ]
+ 
+print "list1[0]: ", list1[0]
+print "list2[1:5]: ", list2[1:5]
+	
+	
+list = []          ## 
+list.append('Google')   ##  append() 
+list.append('Runoob')
+print list
+
+
+list3 = ['physics', 'chemistry', 1997, 2000]
+ 
+print list3
+del list3[2]
+print "After deleting value at index 2 : "
+print list3
