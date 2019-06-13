fb7435d47a15844b8e72fc8d200731d6a4ab5bd9
leaphe <13918825181@qq.com>
2019-06-13 17:45:26 +0800

    Davis

--- git.sh
+++ git.sh
@@ -20,10 +20,3 @@ chmod 777 git.sh
 
 #执行程序
 endtime=date +'%Y-%m-%d %H:%M:%S'
-
-echo ‘endtime’+endtime
-
-start_seconds=$(date --date="$starttime" +%s);
-end_seconds=$(date --date="$endtime" +%s);
-echo "本次运行时间： "$((end_seconds-start_seconds))"s"
- 
