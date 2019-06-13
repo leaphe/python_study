4aeccaec6fc7439597a9fb2bf4c76da1ee7e9451
leaphe <13918825181@qq.com>
2019-06-13 17:54:43 +0800

    Davis

--- git.sh
+++ git.sh
@@ -22,6 +22,15 @@ endtime= date +'%Y-%m-%d %H:%M:%S'
 #echo ‘endtime’+endtime
 
 
+start_time=`date --date='0 days ago' "+%Y-%m-%d %H:%M:%S"`
+#this is your shell script 
+sleep 18
+##############
+finish_time=`date --date='0 days ago' "+%Y-%m-%d %H:%M:%S"`
+duration=$(($(($(date +%s -d "$finish_time")-$(date +%s -d "$start_time")))))
+echo "this shell script execution duration: $duration"
+ 
+
 start_seconds=$(date--date="$starttime" +%s)
 end_seconds=$(date--date="$endtime" +%s)
 echo "本次运行时间： "$((end_seconds-start_seconds))"s"
