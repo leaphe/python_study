6d29c077f77234ec99161b0beab16d6210c254de
leaphe <13918825181@qq.com>
2019-06-13 17:38:32 +0800

    Davis

--- git.sh
+++ git.sh
@@ -1,4 +1,8 @@
 #!bin/sh
+starttime=`date +'%Y-%m-%d %H:%M:%S'`
+
+ 
+
 cd ~/Users/hexizan/documents/python_study
 pwd
 git --version
@@ -12,4 +16,11 @@ git push origin master
 echo '执行成功'
 
 chmod 777 git.sh
+
+
+#执行程序
+endtime=`date +'%Y-%m-%d %H:%M:%S'`
+start_seconds=$(date --date="$starttime" +%s);
+end_seconds=$(date --date="$endtime" +%s);
+echo "本次运行时间： "$((end_seconds-start_seconds))"s"
  
